71ed69f075b1c6c616ae85e80084e5aff50c55d0
Eddie Law <eddielaw296@gmail.com>
2016-11-02 00:18:50 -0400

    add new tests to test that cocos2d environment works properly in karma

--- test/cocos2d/cc.spec.js
+++ test/cocos2d/cc.spec.js
@@ -1,39 +1,24 @@
-
-var canvasObject = document.createElement('canvas');
-        var width = 200;
-        var height = 100;
-        var myCanvas = {};  
-        canvasObject.width = width;
-        canvasObject.height= height;
-        canvasObject.setAttribute("id", "gameCanvas");
-        document.body.appendChild(canvasObject);
-
-cc.game.onStart = function(){
-  console.log("onstart being called");
-  if(!cc.sys.isNative && document.getElementById("cocosLoading")) //If referenced loading.js, please remove it
-    document.body.removeChild(document.getElementById("cocosLoading"));
-  // Pass true to enable retina display, disabled by default to improve performance
-  cc.view.enableRetina(true);
-  // Adjust viewport meta
-  cc.view.adjustViewPort(true);
-  // Setup the resolution policy and design resolution size
-  cc.view.setDesignResolutionSize(800, 450, cc.ResolutionPolicy.SHOW_ALL);
-  // The game will be resized when browser size change
-  cc.view.resizeWithBrowserSize(true);
-  //MainCtrl.start();
-console.log("cc.game.run is being run");
-
+let ccstart = require('./main.js');
+import waitUntil from 'wait-until-promise';
+let addCanvas = function(){
+  let canvasObject = document.createElement('canvas');
+  let width = 800;
+  let height = 800;
+  let myCanvas = {};  
+  canvasObject.width = width;
+  canvasObject.height= height;
+  canvasObject.setAttribute("id", "gameCanvas");
+  document.body.appendChild(canvasObject);
 };
-cc.game.run();
+
+beforeAll((done)=> {
+  addCanvas();
+  ccstart(done);
+});
 
 describe('cc library object', () => {
-  console.log("test is being run");
-  beforeEach(function() {
-  });
   it('should be the cocos2d library object', () => {
-  console.log(cc);
-  console.log(cc.Scene);
-    var sceneObj = {
+    let sceneObj = {
       onEnter:()=>undefined,
       update:(dt)=>dt
     };
@@ -43,10 +28,18 @@ describe('cc library object', () => {
 
   });
 
-  it('should be able to return a sprite', () => {
-    expect(sub).toBe(3);
-    var tSprite = new cc.Sprite("");
-    console.log(tSprite);
+  it('should be able to return a sprite', (done) => {
+    let tSprite = new cc.Sprite(testRes.test);
+    waitUntil(
+      ()=>tSprite.texture,
+      2000
+    ).then(
+      (texture) => {
+        expect(texture.url).toEqual("base/test/res/test.png");
+        done();
+      }
+    );
   });
 
 });
+
