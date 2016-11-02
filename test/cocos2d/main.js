6000fc01ea3ea3e676d32b9ddd14019ad1ca0879
Eddie Law <eddielaw296@gmail.com>
2016-11-02 00:17:35 -0400

    add cocos config files to test dir

--- test/cocos2d/main.js
+++ test/cocos2d/main.js
@@ -46,29 +46,19 @@
  }
  *
  */
-var canvasObject = document.createElement('canvas');
-        var width = 200;
-        var height = 100;
-        var myCanvas = {};  
-        canvasObject.width = width;
-        canvasObject.height= height;
-        canvasObject.setAttribute("id", "gameCanvas");
-        document.body.appendChild(canvasObject);
-
-cc.game.onStart = function(){
-  console.log("onstart being called");
-  if(!cc.sys.isNative && document.getElementById("cocosLoading")) //If referenced loading.js, please remove it
-    document.body.removeChild(document.getElementById("cocosLoading"));
-  // Pass true to enable retina display, disabled by default to improve performance
-  cc.view.enableRetina(true);
-  // Adjust viewport meta
-  cc.view.adjustViewPort(true);
-  // Setup the resolution policy and design resolution size
-  cc.view.setDesignResolutionSize(800, 450, cc.ResolutionPolicy.SHOW_ALL);
-  // The game will be resized when browser size change
-  cc.view.resizeWithBrowserSize(true);
-  //MainCtrl.start();
-
+module.exports = function(done){
+  cc.game.onStart = function(){
+    if(!cc.sys.isNative && document.getElementById("cocosLoading")) //If referenced loading.js, please remove it
+      document.body.removeChild(document.getElementById("cocosLoading"));
+    // Pass true to enable retina display, disabled by default to improve performance
+    cc.view.enableRetina(true);
+    // Adjust viewport meta
+    cc.view.adjustViewPort(true);
+    // Setup the resolution policy and design resolution size
+    cc.view.setDesignResolutionSize(800, 450, cc.ResolutionPolicy.SHOW_ALL);
+    // The game will be resized when browser size change
+    cc.view.resizeWithBrowserSize(true);
+    done();
+  };
+  cc.game.run();
 };
-//console.log("cc.game.run is being run");
-//cc.game.run();
