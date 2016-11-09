2ff6566572e62aa3e003f544b8f556ff411f0db1
Eddie Law <eddielaw296@gmail.com>
2016-11-08 23:14:48 -0500

    tested reusable code (migrated from boilerplate cocos test code)e

---
+++ test/startup/startup.spec.js
@@ -0,0 +1,34 @@
+let cctestutils = require('../../src/cctestutils.js');
+let spriteTest = cctestutils.test.Sprite;
+
+beforeAll((done)=> {
+  cctestutils.startup.addCanvas();
+  cctestutils.startup.ccstart(done);
+});
+
+describe('cc library object', () => {
+  it('should be the cocos2d library object', () => {
+    let sceneObj = {
+      onEnter:()=>undefined,
+      update:(dt)=>dt
+    };
+
+    expect(cc).toBeDefined();
+    expect(cc.Scene.extend(sceneObj)).toBeDefined();
+
+  });
+
+  it('should be able to return a sprite', (done) => {
+    let tSprite = new cc.Sprite(testRes.test);
+    
+    spriteTest.checkEquals(tSprite,testRes.test).then((isSprite)=>{
+      expect(isSprite).toBe(true);
+      done();
+    }).catch(
+      (err)=>{
+        expect(err).toBeNull();
+        done();
+      });
+  });
+});
+
