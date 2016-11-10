45cb653f9664858900d16b98172b9f146b402bf0
Eddie Law <eddielaw296@gmail.com>
2016-11-09 23:00:01 -0500

    change file paths related to cc tests served by karma.conf to point to cctestutils

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -14,13 +14,14 @@ module.exports = function (config) {
       {pattern: 'node_modules/cc2dhtml5/CCBoot.js', watched: false, served: true, included:true},
       {pattern: 'node_modules/cc2dhtml5/**/*.js', watched: false, served: true, included: false},
       {pattern: 'node_modules/cc2dhtml5/moduleConfig.json', watched: false, served: true, included: false},
-      {pattern: 'node_modules/cctestutils/cocos2d/resource.js', watched: false, served: true, included: false},
-      {pattern: 'test/res/**/*.png', watched: false, served: true, included: false},
-      {pattern: 'node_modules/cctestutils/cocos2d/project.json', watched: false, served: true, included: false},
+      {pattern: 'node_modules/cctestutils/cocos2d/**/*.*', watched: false, served: true, included: false},
+      {pattern: 'node_modules/cctestutils/res/**/*.png', watched: false, served: true, included: false},
       'test/tests.bundle.js',
     ],
     proxies: {
       '/project.json':'/base/node_modules/cctestutils/cocos2d/project.json',
+      '/base/test/resource.js':'/base/node_modules/cctestutils/cocos2d/resource.js',
+      '/base/test/res/':'/base/node_modules/cctestutils/res/',
       '/frameworks/cocos2d-html5/':'/base/node_modules/cc2dhtml5/'
     },
     frameworks: [
